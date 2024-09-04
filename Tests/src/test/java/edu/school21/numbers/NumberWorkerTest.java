54d6242a409efca2416fb1b42ee58852c2a7daee
umr006 <umar.ausheff@yandex.ru>
2024-09-04 23:25:55 +0500

    ex00

--- Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
+++ Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -1,4 +1,21 @@
 package edu.school21.numbers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import edu.school21.numbers.NumberWorker;
+
 public class NumberWorkerTest {
+    NumberWorker numberWorker = new NumberWorker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 7, 101, 103, 821})
+    void isPrimeForPrimes(int numb) {
+        assertTrue(numberWorker.isPrime(numb));
+    }
+
 }
